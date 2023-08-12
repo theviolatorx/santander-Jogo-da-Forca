d8ea3adc82b61229609b6513a795614c473c52d9
Clovis Garcia <theviolatorx@gmail.com>
2023-08-12 18:59:30 -0300

    Update evento key press or button

--- engine.js
+++ engine.js
@@ -8,6 +8,10 @@ document.addEventListener("keydown", teclaPressionada);
 
 secretWord = "TESTE";
 
+function isCaracter(keyCaracter) {
+  return keyCaracter >=65 && keyCaracter <= 90;
+}
+
 function teclaPressionada(evento) {
   //   if (!evento.keyCode) {
   //     console.log(`Clicou em ${evento.target.id}`);
@@ -15,30 +19,42 @@ function teclaPressionada(evento) {
   //     console.log(`Apertou a tecla ${evento.key} de código ${evento.keyCode} `);
   //   }
 
-  // Verifica qual dos dois eventos foi utilizado.
-  // O que retorna null/undefine é ignorado.
-  let letra = evento.key ?? String.fromCharCode(evento.target.id);
-  // Converte para maíuscula a letra clicada ou digitada
-  letra = letra.toUpperCase();
+  // Verifica qual dos dos dois eventos foi utilizado:
+  // tecla pressioada ou click em um botão.
+  // E retorna o código da tecla ou botão clicado.
+  const keyPressClick = parseInt(evento.keyCode ?? evento.target.id);
+  
 
-  // Verifica se a letra clicada/pressionada esta na palavra secreta.
-  if (secretWord.includes(letra)) {
-    // Verifica se a letra correta esta inclusa na relação de palabras corretas.
-    // Caso já esteja, não adiciona novamente a mesma letra.
-    if (!rightword.includes(letra)) {
-      // Caso a letra não esteja na lista de palavras corretas,
-      // a mesma é adicionada.
-      rightword.push(letra);
-      console.log("Letra Correta: " + rightword);
-    }
-  } else {
-    // Verifica se a letra errada esta inclusa na relação de palabras erradas.
-    // Caso já esteja, não adiciona novamente a mesma letra.
-    if (!wrongword.includes(letra)) {
-      // Caso a letra não esteja na lista de palavras erradas,
-      // a mesma é adicionada.
-      wrongword.push(letra);
-      console.log("Letra Errada: " + wrongword);
+  // Verifica se o código do evento é um caracter válido.
+  if (isCaracter(keyPressClick)) {
+
+    // Verifica qual dos dois eventos foi utilizado.
+    // O que retorna null/undefine é ignorado.
+    let letra = evento.key ?? String.fromCharCode(evento.target.id);
+
+    // Converte para maíuscula a letra clicada ou digitada
+    letra = letra.toUpperCase();
+
+    // Verifica se a letra clicada/pressionada esta na palavra secreta.
+    if (secretWord.includes(letra)) {
+      // Verifica se a letra correta esta inclusa na relação de palabras corretas.
+      // Caso já esteja, não adiciona novamente a mesma letra.
+      if (!rightword.includes(letra)) {
+        // Caso a letra não esteja na lista de palavras corretas,
+        // a mesma é adicionada.
+        rightword.push(letra);
+        console.log("Letra Correta: " + rightword);
+      }
+    } else {
+      // Verifica se a letra errada esta inclusa na relação de palabras erradas.
+      // Caso já esteja, não adiciona novamente a mesma letra.
+      if (!wrongword.includes(letra)) {
+        // Caso a letra não esteja na lista de palavras erradas,
+        // a mesma é adicionada.
+        wrongword.push(letra);
+        console.log("Letra Errada: " + wrongword);
+      }
     }
   }
 }
+
